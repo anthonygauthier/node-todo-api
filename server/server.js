dee0a86a26c91b9f8fb94fd3beb524296421e8d6
Anthony Gauthier <antho325@hotmail.com>
2018-01-21 01:29:48 -0500

    mongoose p2

--- server/server.js
+++ server/server.js
@@ -5,24 +5,46 @@ mongoose.connect('mongodb://localhost:27017/Todoapp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-var newTodo = new Todo({
-    text: 'Do something',
-    completed: true,
-    completeAt: 1827398
+// var newTodo = new Todo({
+//     text: '   Edit this video   '
+// });
+
+// newTodo.save().then((doc)=> {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1
+    }
 });
 
-newTodo.save().then((doc)=> {
-    console.log('Saved todo', doc);
+var newUser = new User({
+    email: ' '
+})
+
+newUser.save().then((doc)=> {
+    console.log(JSON.stringify(doc, undefined, 2));
 }, (e) => {
-    console.log('Unable to save todo');
+    console.log('Unable to save user');
 });
