5906eb822c8d3b1e611cf5b58ede3a6ab45c3e15
Anthony Gauthier <antho325@hotmail.com>
2018-01-21 21:54:10 -0500

    added express and installed postman

--- server/server.js
+++ server/server.js
@@ -1,45 +1,33 @@
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minLength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400);
+        res.send(e);
+    });
 });
 
-// var newTodo = new Todo({
-//     text: '   Edit this video   '
-// });
+// app.post('/users', (req, res) => {
+//     var user = new User({
 
-// newTodo.save().then((doc)=> {
-//     console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//     console.log('Unable to save todo');
-// });
+//     })
+// })
 
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 1
-    }
-});
-
-var newUser = new User({
-    email: ' '
-})
-
-newUser.save().then((doc)=> {
-    console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-    console.log('Unable to save user');
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
