0b3c7af9ec8ec8b9356b3c9f216f2800a2a23497
Anthony Gauthier <antho325@hotmail.com>
2018-02-18 16:21:22 -0500

    Added test cases for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,3 +98,38 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+   it('should delete todo doc', (done) => {
+       request(app)
+        .delete(`/todos/${todos[1]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(todos[1]._id.toHexString());
+        })
+        .end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+
+            Todo.findById(todos[1]._id.toHexString()).then(todo => {
+                expect(todo).toBe(null);
+                done();
+            }).catch((err) => done(err));
+        });
+   });
+
+   it('should return 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+   });
+
+   it('should return a 404 for non-object ids', (done) => {
+        request(app)
+            .delete(`/todos/123`)
+            .expect(404)
+            .end(done);
+   });
+});
