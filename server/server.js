5d0612c40fe6faf93420d0f61a41173d06cf3f5e
Anthony Gauthier <antho325@hotmail.com>
2018-03-05 15:23:52 -0500

    Setting up the user model

--- server/server.js
+++ server/server.js
@@ -93,7 +93,18 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(port, () => {
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(port, "0.0.0.0", () => {
     console.log('Started on port ', port);
 });
 
